baaf49c7d62071d11697223a986185604e09f0a2
Kimberly <83323970+kimbcheh@users.noreply.github.com>
2021-05-31 09:23:32 +1000

    feat: Added a loading state (#1)
    * feat: basic loading state
    
    * feat: set loading to false once BOTH price and time data is fetched
    
    * chore: cleanup before squash
    
    * docs: removed todo comments

--- src/App.js
+++ src/App.js
@@ -1,53 +1,45 @@
 import './App.css'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import Price from './components/Price'
-
-// Not using an env because no authentication is required for API
-// TODO: Implement other currencies `https://api.coinbase.com/v2/prices/spot?currency=${chosenCurrency || USD}` - will this work? Default will be USD
-// TODO: Implement an error to show if no data
-// TODO: Implement refresh every minute
-// TODO: Implement a chart (Angus suggested Plotly)
-// TODO: Maybe implement useReducer() for the states?
-// I think the app tries to run before data has loaded - Implement a loading state?
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
   const [priceData, setPriceData] = useState()
   const [timeData, setTimeData] = useState()
 
   useEffect(() => {
-    console.log('initialising interval')
+    // Set to loading again on each refresh
+    setIsLoading(true)
+
+    // Initialising interval of 60 seconds
     const interval = setInterval(() => {
-      fetchPriceData()
-      fetchTimeData()
+      fetchData()
     }, 60000)
 
-    // To fetch BTC spot price data
-    const fetchPriceData = async () => {
+    // Fetch price and time data
+    const fetchData = async () => {
       try {
-        const response = await axios.get(
-          'https://api.coinbase.com/v2/prices/spot?currency=USD'
-        )
-        console.log(response.data.data)
-        setPriceData(response.data.data)
+        const pricePromise = () => {
+          return axios.get(
+            'https://api.coinbase.com/v2/prices/spot?currency=USD'
+          )
+        }
+        const timePromise = () => {
+          return axios.get('https://api.coinbase.com/v2/time')
+        }
+        await Promise.all([pricePromise(), timePromise()]).then((response) => {
+          setPriceData(response[0].data.data.amount)
+          setTimeData(response[1].data.data.iso)
+          setIsLoading(false)
+        })
       } catch (error) {
         console.error(error)
       }
     }
-    fetchPriceData()
 
-    // To fetch time at API server
-    const fetchTimeData = async () => {
-      try {
-        const response = await axios.get('https://api.coinbase.com/v2/time')
-        console.log(response.data.data.iso)
-        setTimeData(response.data.data.iso)
-      } catch (error) {
-        console.error(error)
-      }
-    }
-    fetchTimeData()
+    fetchData()
 
+    // Clear interval set
     return () => {
       clearInterval(interval)
     }
@@ -56,9 +48,14 @@ function App() {
   return (
     <div>
       <h1>BTC Tracker</h1>
-      {/* <p>{priceData.amount}</p>
-      <p>as of {timeData}</p> */}
-      <Price />
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <div>
+          <p>{priceData}</p>
+          <p>as at {timeData}</p>
+        </div>
+      )}
     </div>
   )
 }
